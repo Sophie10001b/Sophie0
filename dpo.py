6e427ccd53ee2f062f6fc4a6d49af2792d274b07
Sophie100001b <Hhz029@hotmail.com>
2025-04-27 10:58:37 +0800

    要提交的变更： 	新文件：   dpo.py 	修改：     sft.py 	新文件：   test/test_pretrain.ipynb

--- dpo.py
+++ dpo.py
@@ -36,7 +36,7 @@ torch.set_float32_matmul_precision("medium")
 HF_CACHE = "/root/autodl-tmp/hf_cache"
 os.environ["HF_HOME"] = HF_CACHE
 
-class SFTDataset(torch.utils.data.Dataset):
+class DPODataset(torch.utils.data.Dataset):
     def __init__(self, tokenizer: AutoTokenizer, model_config: PretrainedConfig, train_config: argparse.Namespace, **kwargs):
         super().__init__(**kwargs)
 
@@ -45,7 +45,7 @@ class SFTDataset(torch.utils.data.Dataset):
         self.train_config = train_config
 
         data_files = glob.glob(self.train_config.data_path + "/**/*.parquet", recursive=True)
-        print(f"sft data size: {sum([os.path.getsize(_) / (1024 * 1024) for _ in data_files]):.4f} MB\n")
+        print(f"dpo data size: {sum([os.path.getsize(_) / (1024 * 1024) for _ in data_files]):.4f} MB\n")
 
         datas: Dataset = load_dataset("parquet", data_files=data_files, split="train", streaming=False, trust_remote_code=True, columns=["conversations"], cache_dir=HF_CACHE, num_proc=32)
         datas = datas.shuffle(seed=self.train_config.seed)
@@ -57,7 +57,7 @@ class SFTDataset(torch.utils.data.Dataset):
             batch_size=5000,
             num_proc=32,
             remove_columns=datas.column_names,
-            cache_file_name=os.path.join(HF_CACHE, "parquet/sft/sft_map.cache")
+            cache_file_name=os.path.join(HF_CACHE, "parquet/dpo/dpo_map.cache")
         )
     
     def _preprocess(self, raw):
@@ -139,14 +139,14 @@ class SFTDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.datas)
 
-class SFTDataModule(LightningDataModule):
+class DPODataModule(LightningDataModule):
     def __init__(self, tokenizer: AutoTokenizer, model_config: PretrainedConfig, train_config: argparse.Namespace, **kwargs):
         super().__init__(**kwargs)
 
         self.model_config = model_config
         self.train_config = train_config
 
-        self.data = SFTDataset(tokenizer, model_config, train_config, **kwargs)
+        self.data = DPODataset(tokenizer, model_config, train_config, **kwargs)
     
     def setup(self, stage):
         pass
@@ -164,7 +164,7 @@ class SFTDataModule(LightningDataModule):
 #########################################################
 #                  --- model ---
 #########################################################
-class SFTModule(LightningModule):
+class DPOModule(LightningModule):
     def __init__(self, tokenizer: AutoTokenizer, model: PreTrainedModel, model_config: PretrainedConfig, train_config: argparse.Namespace, **kwargs):
         super().__init__(**kwargs)
 
@@ -286,7 +286,7 @@ def main(train_config: argparse.Namespace):
         default_root_dir=train_config.ckpt_path,
         accumulate_grad_batches=train_config.accumulate_grad_batches,
         gradient_clip_val=1.0,
-        logger=TensorBoardLogger(logger_path, name="sft"),
+        logger=TensorBoardLogger(logger_path, name="dpo"),
         callbacks=ModelCheckpoint(
             every_n_epochs=1 if train_config.save_steps == -1 else None,
             every_n_train_steps=train_config.save_steps if train_config.save_steps != -1 else None,
@@ -304,37 +304,37 @@ def main(train_config: argparse.Namespace):
 
     model = Sophie0ForCausalLM(model_config)
     tokenizer: AutoTokenizer = AutoTokenizer.from_pretrained(os.path.join(_dir, "model", "tokenizer"), use_fast=True, trust_remote_code=True, local_files_only=True)
-    datamodule = SFTDataModule(tokenizer, model_config, train_config)
+    datamodule = DPODataModule(tokenizer, model_config, train_config)
 
     if train_config.max_steps == -1: train_config.max_steps = ((len(datamodule.data) + raw_batch_size - 1) // raw_batch_size) * train_config.max_epochs
 
     if train_config.pretrained_ckpt_path != "": model.load_state_dict(torch.load(train_config.pretrained_ckpt_path, map_location='cpu', weights_only=True))
-    plmodel = SFTModule(tokenizer, model, model_config, train_config)
+    plmodel = DPOModule(tokenizer, model, model_config, train_config)
     trainer.fit(plmodel, datamodule=datamodule)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # dataset Args:
-    sft_parser = parser.add_argument_group("sft")
-    sft_parser.add_argument("--seed", type=int, default=17)
-    sft_parser.add_argument("--data_path", type=str, help="Path to the dataset dir", default="data/sft")
-    sft_parser.add_argument("--ckpt_path", type=str, help="Path to the checkpoint", default="result/sft")
-    sft_parser.add_argument("--pretrained_ckpt_path", type=str, default="", help="Path to the pretrained checkpoint")
+    dpo_parser = parser.add_argument_group("dpo")
+    dpo_parser.add_argument("--seed", type=int, default=17)
+    dpo_parser.add_argument("--data_path", type=str, help="Path to the dataset dir", default="data/dpo")
+    dpo_parser.add_argument("--ckpt_path", type=str, help="Path to the checkpoint", default="result/dpo")
+    dpo_parser.add_argument("--pretrained_ckpt_path", type=str, default="", help="Path to the pretrained checkpoint")
 
-    sft_parser.add_argument("--max_seqlen", type=int, default=2048)
-    sft_parser.add_argument("--max_token_per_batch", type=int, default=524288)
-    sft_parser.add_argument("--batch_size", type=int, default=1)
-    sft_parser.add_argument("--accumulate_grad_batches", type=int, default=1, help="Accumulate gradients for every n batches")
-    sft_parser.add_argument("--num_workers", type=int, default=4)
+    dpo_parser.add_argument("--max_seqlen", type=int, default=2048)
+    dpo_parser.add_argument("--max_token_per_batch", type=int, default=524288)
+    dpo_parser.add_argument("--batch_size", type=int, default=1)
+    dpo_parser.add_argument("--accumulate_grad_batches", type=int, default=1, help="Accumulate gradients for every n batches")
+    dpo_parser.add_argument("--num_workers", type=int, default=4)
 
-    sft_parser.add_argument("--max_steps", type=int, default=-1)
-    sft_parser.add_argument("--max_epochs", type=int, default=1)
-    sft_parser.add_argument("--save_steps", type=int, default=-1)
-    sft_parser.add_argument("--max_lr", type=float, default=1e-4, help="Maximum learning rate")
-    sft_parser.add_argument("--min_lr", type=float, default=1e-6, help="Minimum learning rate")
-    sft_parser.add_argument("--warmup_ratio", type=float, default=0.1, help="Ratio of steps to warm up learning rate")
-    sft_parser.add_argument("--precision", type=str, default="bf16-mixed")
+    dpo_parser.add_argument("--max_steps", type=int, default=-1)
+    dpo_parser.add_argument("--max_epochs", type=int, default=1)
+    dpo_parser.add_argument("--save_steps", type=int, default=-1)
+    dpo_parser.add_argument("--max_lr", type=float, default=1e-4, help="Maximum learning rate")
+    dpo_parser.add_argument("--min_lr", type=float, default=1e-6, help="Minimum learning rate")
+    dpo_parser.add_argument("--warmup_ratio", type=float, default=0.1, help="Ratio of steps to warm up learning rate")
+    dpo_parser.add_argument("--precision", type=str, default="bf16-mixed")
 
     args = parser.parse_args()
 
